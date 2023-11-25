61faa0153dc3a55987d1c5b79529dd192f35c058
Naninho Carioca <cgferrarotti@gmail.com>
2023-11-25 15:07:10 -0300

    configuro node (sólo devuelve el index desde app.js)

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html')
+});
+
+app.get('/ping', (req, res) => {
+    res.send('pong')
+});
+
+app.listen(4000, () => {
+    console.log('Servidor corriendo en http:\\\\localhost:4000')
+});
